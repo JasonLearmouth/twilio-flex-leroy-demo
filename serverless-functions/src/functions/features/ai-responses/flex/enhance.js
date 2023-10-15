1af91cc4377297ace094a3f8a47989c1afccc84b
Leroy Chan <5236195+leroychan@users.noreply.github.com>
2023-10-15 17:37:59 +0800

    fix: update source code to fit openai v4

--- serverless-functions/src/functions/features/ai-responses/flex/enhance.js
+++ serverless-functions/src/functions/features/ai-responses/flex/enhance.js
@@ -1,24 +1,18 @@
-const { Configuration, OpenAIApi } = require('openai');
+const { OpenAI } = require('openai');
 const { prepareFlexFunction } = require(Runtime.getFunctions()['common/helpers/function-helper'].path);
 const requiredParameters = ['open_ai_request'];
 
 exports.handler = prepareFlexFunction(requiredParameters, async (context, event, callback, response, handleError) => {
   try {
-    const openAiConfiguration = new Configuration({
+    const openai = new OpenAI({
       apiKey: context.OPENAI_API_KEY,
     });
 
-    const openaiClient = new OpenAIApi(openAiConfiguration);
-
-    console.log(`Open AI Key ${context.OPENAI_API_KEY} with model ${context.OPENAI_MODEL}`);
-
-    const completion = await openaiClient.createChatCompletion({
+    const completion = await openai.chat.completions.create({
       model: context.OPENAI_MODEL,
       messages: event.open_ai_request,
-      max_tokens: 500,
     });
 
-    console.log('Open AI response', completion.data);
     response.setStatusCode(200);
     response.setBody({ result: completion.data });
 
